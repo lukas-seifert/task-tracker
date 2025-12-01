71c4d5797b1e743e1bc7c81a44ab9295608731d6
Lukas Seifert <lukas-seifert@outlook.de>
2025-12-01 21:16:14 +0100

    feat: add task domain model (entity, enums)

---
+++ src/main/java/com/example/task_tracker/task/model/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.example.task_tracker.task.model;
+
+public enum TaskPriority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/example/task_tracker/task/model/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.example.task_tracker.task.model;
+
+public enum TaskStatus {
+    OPEN,
+    IN_PROGRESS,
+    DONE
+}
